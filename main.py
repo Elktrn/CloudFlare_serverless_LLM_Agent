073a634936d0b49d9dfabb6aae2688a2ce1ec799
Mohammad Asadolahi <mohammad.e.asadolahi@gmail.com>
2025-08-07 20:10:48 +0330

    Test if opeanai key is added to the cloudflare secrets correctly

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from workers import Response
 import json
 
-def on_fetch(request):
+def on_fetch(request, env):
     if request.method == "POST":
+        openai_api_key = env.openai_api_key
         try:
             # Read the JSON payload
             payload = request.json()
             name = payload.get("name", "Unknown")
             processed_data = {
-                "greeting": f"Hello, {name}!"}
+                "greeting": f"Hello, {name}!","mykey":openai_api_key[0:10]}
+            print(openai_api_key[0:10])
             return Response(json.dumps(processed_data), status=200)
         except Exception as e:
             error_data = {"error": f"Failed to process POST request: {str(e)}"}
