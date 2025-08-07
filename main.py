1371029c180453adb1b2a2a835c0fac2a7339a01
Mohammad Asadolahi <mohammad.e.asadolahi@gmail.com>
2025-08-07 19:58:12 +0330

    Exception handling has been added fo rthe POST method and json dump

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from workers import Response
 import json
 
 def on_fetch(request):
-    data = {
-        "message": "Hello Python Worker!",
-        "status": "completed request"
-    }
-
-    json_data = json.dumps(data)
-
-    headers = {
-        "content-type": "application/json"
-    }
-    return Response(json_data, headers=headers)
+    if request.method == "POST":
+        try:
+            # Read the JSON payload
+            payload = request.json()
+            name = payload.get("name", "Unknown")
+            processed_data = {
+                "greeting": f"Hello, {name}!"}
+            return Response(json.dumps(processed_data), status=200)
+        except Exception as e:
+            error_data = {"error": f"Failed to process POST request: {str(e)}"}
+            return Response(json.dumps(error_data), status=400)
+    else:
+        data = {
+            "error": "Failed to process Non-POST request"
+        }
+        return Response(json.dumps(data), status=400)
