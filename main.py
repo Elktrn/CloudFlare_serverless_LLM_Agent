55f3d7efbe07446dea6de96612ac6df1a033375b
https://github.com/Elktrn <79268727+Elktrn@users.noreply.github.com>
2025-08-07 21:59:47 +0330

    Update main.py
    Checkour to the working version

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from workers import Response
 import json
 
-def on_fetch(request, env):
-    # print(env)
+def on_fetch(request):
     if request.method == "POST":
-        openai_api_key = hhhsgsdfgsdfgsdfgsdfgsghhh#env.openaikey
         try:
             # Read the JSON payload
             payload = request.json()
             name = payload.get("name", "Unknown")
             processed_data = {
-                "greeting": f"Hello, {name}!"}#,"mykey":openai_api_key[0:10]}
+                "greeting": f"Hello, {name}!"}
             return Response(json.dumps(processed_data), status=200)
         except Exception as e:
             error_data = {"error": f"Failed to process POST request: {str(e)}"}
             return Response(json.dumps(error_data), status=400)
     else:
         data = {
-            "error": "Failed to process Non-POST request"
+            "error": "Failed  to process Non-POST request"
         }
         return Response(json.dumps(data), status=400)
