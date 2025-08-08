3e1194270e43c5ca8a465df3028d3652ecaeab71
Mohammad Asadolahi <mohammad.e.asadolahi@gmail.com>
2025-08-08 07:56:44 +0330

    Return JobID at the request

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from workers import Response
 import json
+import uuidv4;
 
 async def on_fetch(request,env):
     if request.method == "POST":
         try:
             #env.openaikey
+            jobId = uuidv4()
+            immediate_response = {
+                "jobId": jobId,
+                "status": "accepted"
+            }
             pyaload = await request.json()
-            name =pyaload.name
+            name =pyaload.durationDays
             processed_data = {
-                "greeting": f"Hello, {name}!"}
+                "greeting": f"DurationDays, {name}!"}
             return Response(json.dumps(processed_data), status=200)
         except Exception as e:
             error_data = {"error": f"Failed to process POST request: {str(e)}"}
