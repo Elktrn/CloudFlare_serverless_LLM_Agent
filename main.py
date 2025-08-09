673a6b4613923cc3e1fbfaadc5f76d63e5ad335d
Mohammad Asadolahi <mohammad.e.asadolahi@gmail.com>
2025-08-09 22:10:49 +0330

    Debug code added

--- main.py
+++ main.py
@@ -19,30 +19,29 @@ async def on_fetch(request, env):
                 job_id = 'twert'# await str(uuid.uuid4())
                 
                 # Register job in KV storage
-                # processed_data = {
-                #     "jobId": job_id,
-                #     "destination": payload.destination,
-                #     "durationDays": payload.durationDays,
-                #     "status": "pending",
-                #     "createdAt": str(datetime.datetime.now()),
-                #     "completedAt": "null",
-                #     "itinerary": "null",
-                #     "error": "null"
-                # }
+                processed_data = {
+                    "jobId": job_id,
+                    "destination": payload.destination,
+                    "durationDays": payload.durationDays,
+                    "status": "pending",
+                    "createdAt": str(datetime.datetime.now()),
+                    "completedAt": "null",
+                    "itinerary": "null",
+                    "error": "null"
+                }
                 # json_data =await json.dumps(processed_data)
                 # await env.itinerarykv.put(f"job_{job_id}", json_data)
                 
                 ite=[]
                 # for i in range(1,payload.durationDays+1):
-                for i in range(1,2+1):
-                    ite.append({"day":i,"theme":"FILL","activities":[{"time":"morning","description":"FILL","location":"FILL"},{"time":"afternoon","description":"FILL","location":"FILL"},{"time":"evening","description":"FILL","location":"FILL"}]})
+                    # ite.append({"day":i,"theme":"FILL","activities":[{"time":"morning","description":"FILL","location":"FILL"},{"time":"afternoon","description":"FILL","location":"FILL"},{"time":"evening","description":"FILL","location":"FILL"}]})
 
                 # env.fetch(
                 #         "https://llm-itinerary-generator-processor.mohammad-e-asadolahi.workers.dev/",
                 #         method="POST",
                 #         body={"jobId": job_id,"iten":str(ite)})
-
-                return Response(str(ite), status=202)
+                pd =await json.loads(processed_data)
+                return Response(json.dumps(pd), status=202)
             except Exception as e:
                 error_data = {"error": f"Failed to process POST request: {str(e)}"}
                 return Response(json.dumps(error_data), status=400)
