a7d368459876dae07e8b49495d6ccc6bce3cf99c
Mohammad Asadolahi <mohammad.e.asadolahi@gmail.com>
2025-08-09 18:58:01 +0330

    Payload get update in the itenerary json generation loop has been updated

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def on_fetch(request, env):
                 await env.itinerarykv.put(f"job_{job_id}", json_data)
                 
                 ite=[]
-                for i in range(1,payload.get("durationDays")+1):
+                for i in range(1,payload.durationDays+1):
                     ite.append({"day":i,"theme":"FILL","activities":[{"time":"morning","description":"FILL","location":"FILL"},{"time":"afternoon","description":"FILL","location":"FILL"},{"time":"evening","description":"FILL","location":"FILL"}]})
 
                 env.fetch(
