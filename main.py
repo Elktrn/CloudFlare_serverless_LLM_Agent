ccb08e089345f8bfbcda127a6d3139861c33a061
Mohammad Asadolahi <mohammad.e.asadolahi@gmail.com>
2025-08-10 20:41:59 +0330

    Execption handling to correctly check for the jobId if exists

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ async def on_fetch(request, env,ctx):
     if request.method == "POST":
         payload = await request.json()
         try:
-            if payload.jobId !=None:
-                # Check if the request contains a jobId (user is checking job status)
-                job_id = payload.jobId
-                jsond = await env.itinerarykv.get(f"job_{job_id}")
-                parsed_data =await json.loads(jsond)
-                return Response(json.dumps(parsed_data), status=202)
-            else:
+            # Check if the request contains a jobId (user is checking job status)
+            job_id = payload.jobId
+            jsond = await env.itinerarykv.get(f"job_{job_id}")
+            parsed_data =await json.loads(jsond)
+            return Response(json.dumps(parsed_data), status=202)
+        except Exception as e:
+            try:
                 # No jobId, so register a new job
 
                     job_id = await str(uuid.uuid4())
@@ -60,9 +60,9 @@ async def on_fetch(request, env,ctx):
                     #         body={"jobId": job_id,"iten":str(ite)})
                     pd =await json.loads(processed_data)
                     return Response(json.dumps(pd), status=202)
-        except Exception as e:
-            error_data = {"error": f"Failed to process POST request: {str(e)}"}
-            return Response(json.dumps(error_data), status=400)
+            except Exception as e:
+                error_data = {"error": f"Failed to process POST request: {str(e)}"}
+                return Response(json.dumps(error_data), status=400)
     else:
         data = {"error": "Failed to process non-POST request"}
         return Response(json.dumps(data), status=500)
