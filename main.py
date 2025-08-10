1dc05f2940c9492c379ee317b613360a588786d0
Mohammad Asadolahi <mohammad.e.asadolahi@gmail.com>
2025-08-10 21:55:08 +0330

    Adding wait_until

--- main.py
+++ main.py
@@ -39,9 +39,8 @@ async def on_fetch(request, env,ctx):
                     #             "Content-Type": "application/json"
                     #         },
                     # Use ctx.waitUntil to ensure the background task completes
-                    background_task = fetch(api_url,method="POST",body= json.dumps({"jobId": job_id,"iten": str(fabricated_itinerary)}))
                     
-                    # ctx.wait_until(background_task)
+                    ctx.wait_until(fetch(api_url,method="POST",body= json.dumps({"jobId": job_id,"iten": str(fabricated_itinerary)})))
 
                     # env.fetch(
                     #         "https://llm-itinerary-generator-processor.mohammad-e-asadolahi.workers.dev/",
